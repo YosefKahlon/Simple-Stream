dff0584a17b2424204f79a4fab79bc80c0d9fbeb
yosefkahlon <yosefkahlon53@gmail.com>
2022-04-14 21:46:51 +0300

    server / clinet

--- cmake-build-debug/CMakeFiles/Simple_Stream.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Simple_Stream.dir/DependInfo.cmake
@@ -1,8 +1,18 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/97252/Documents/GitHub/Simple Stream/client.c" "/mnt/c/Users/97252/Documents/GitHub/Simple Stream/cmake-build-debug/CMakeFiles/Simple_Stream.dir/client.c.o"
+  "/mnt/c/Users/97252/Documents/GitHub/Simple Stream/server.c" "/mnt/c/Users/97252/Documents/GitHub/Simple Stream/cmake-build-debug/CMakeFiles/Simple_Stream.dir/server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/mnt/c/Users/97252/Documents/GitHub/Simple Stream/main.cpp" "/mnt/c/Users/97252/Documents/GitHub/Simple Stream/cmake-build-debug/CMakeFiles/Simple_Stream.dir/main.cpp.o"
   )
--- cmake-build-debug/CMakeFiles/Simple_Stream.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Simple_Stream.dir/cmake_clean.cmake
@@ -1,10 +1,12 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/Simple_Stream.dir/client.c.o"
   "CMakeFiles/Simple_Stream.dir/main.cpp.o"
+  "CMakeFiles/Simple_Stream.dir/server.c.o"
   "Simple_Stream"
   "Simple_Stream.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/Simple_Stream.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
