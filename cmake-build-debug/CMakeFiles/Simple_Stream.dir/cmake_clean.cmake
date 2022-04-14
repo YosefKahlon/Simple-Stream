947587da0fbd3ec95d3959f3208143842517f2a6
yosefkahlon <yosefkahlon53@gmail.com>
2022-04-14 21:27:35 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Simple_Stream.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simple_Stream.dir/main.cpp.o"
+  "Simple_Stream"
+  "Simple_Stream.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simple_Stream.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
